5b10cfb2f6779f3f63a833beffb91c6425b596c0
Ursula Montag <montagu666@gmail.com>
2022-04-08 16:38:58 +0200

    Add api fetch and function

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-import './style.css'
-
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import "./style.css";
+const appContainer = document.querySelector("#app");
+appContainer.innerHTML = `
+  <button data-js="fetchButton">Fetch</button>
+`;
+const fetchButton = document.querySelector('[data-js="fetchButton"]');
+const urlApi = "https://rickandmortyapi.com/api/character";
+fetchButton.addEventListener("click", () => {
+  async function apiData(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data.results);
+      return characters(data.results);
+    } catch (error) {
+      console.error("Oops, Fehler");
+    }
+  }
+  apiData(urlApi);
+  function characters(charData) {
+    const listContainer = document.createElement("ul");
+    appContainer.append(listContainer);
+    charData.forEach((character) => {
+      const listItem = document.createElement("li");
+      listItem.innerHTML = `
+      <img src="${character.image}"/>
+      <p>${character.name}</p>`;
+      listContainer.append(listItem);
+    });
+  }
+});
