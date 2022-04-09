a41f1d9a631784f221c09a69548b928e7dc994f6
raimartontsch <raimar.tontsch@gmail.com>
2022-04-09 17:55:44 +0200

    Add filter function

--- main.js
+++ main.js
@@ -1,7 +1,46 @@
 import "./style.css";
 const appContainer = document.querySelector("#app");
+let currentFilter = "all";
 appContainer.innerHTML = `
   <button data-js="fetchButton">Fetch</button>
+  <form data-js="filter-form" aria-labeledby="form-heading">
+    <fieldset>
+      <legend id="form-heading">Filter by tags:</legend>
+      <input
+        name="tag-filter"
+        value="all"
+        class="tag-input"
+        id="tag-all"
+        type="radio"
+        checked
+      />
+      <label class="tag-label" for="tag-all">all</label>
+      <input
+        name="tag-filter"
+        value="Alive"
+        class="tag-input"
+        id="tag-alive"
+        type="radio"
+      />
+      <label class="tag-label" for="tag-alive">Alive</label>
+      <input
+        name="tag-filter"
+        value="Dead"
+        class="tag-input"
+        id="tag-dead"
+        type="radio"
+      />
+      <label class="tag-label" for="tag-dead">Dead</label>
+      <input
+        name="tag-filter"
+        value="unknown"
+        class="tag-input"
+        id="tag-unknown"
+        type="radio"
+      />
+      <label class="tag-label" for="tag-unknown">unknown</label>
+    </fieldset>
+  </form>
 `;
 const fetchButton = document.querySelector('[data-js="fetchButton"]');
 const urlApi = "https://rickandmortyapi.com/api/character";
@@ -17,15 +56,30 @@ fetchButton.addEventListener("click", () => {
     }
   }
   apiData(urlApi);
-  function characters(charData) {
-    const listContainer = document.createElement("ul");
-    appContainer.append(listContainer);
-    charData.forEach((character) => {
+});
+
+const filterForm = document.querySelector("[data-js=filter-form]");
+
+function characters(charData) {
+  const listContainer = document.createElement("ul");
+  appContainer.append(listContainer);
+  charData
+    .filter(
+      (character) =>
+        character.status.includes(currentFilter) || currentFilter === "all"
+    )
+    .forEach((character) => {
       const listItem = document.createElement("li");
       listItem.innerHTML = `
-      <img src="${character.image}"/>
-      <p>${character.name}</p>`;
+    <img src="${character.image}"/>
+    <p>${character.name}</p>`;
       listContainer.append(listItem);
     });
-  }
+}
+
+filterForm.addEventListener("change", () => {
+  currentFilter = filterForm.elements["tag-filter"].value;
+
+  const data = apiData(urlApi);
+  characters(data);
 });
